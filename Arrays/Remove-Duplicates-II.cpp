e4339e4e93c7f78da6dcd97d556ee7dc5a894c95
adityafurange <adityafurange3@gmail.com>
2026-05-25 12:50:15 +0530

    Add function to remove excess duplicates from array
    This implementation allows up to two duplicates of each number in the array, ensuring that the array remains sorted while removing excess duplicates.

---
+++ Arrays/Remove-Duplicates-II.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+
+        int n = nums.size();
+
+        if(n < 3)
+            return n;
+
+        int s = 2;
+        int f = 2;
+
+        while(f < n)
+        {
+            if(nums[f] != nums[s - 2])
+            {
+                nums[s++] = nums[f];
+            }
+
+            f++;
+        }
+
+        return s;
+    }
+};
